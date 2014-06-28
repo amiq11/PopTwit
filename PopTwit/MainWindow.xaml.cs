2dcc16fc0bfc66b19a3f44519c04bd02d166da59
Makoto Shimazu <makoto.shimaz@gmail.com>
2014-06-28 17:22:38 +0900

    Tweet from the box

--- PopTwit/MainWindow.xaml.cs
+++ PopTwit/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Tweetinvi;
 
 namespace PopTwit
 {
@@ -20,9 +21,53 @@ namespace PopTwit
     /// </summary>
     public partial class MainWindow : Window
     {
+        private string consumerKey    = "Ul0HawBthgiGnd7ar1qtJfIRX";
+        private string consumerSecret = "JzSYSA7wvYGL3qsJqpNcSFDgIuxgW8NFH3w8Bq71enVzbJlxaD";
+        private string accessToken    = "196993405-2KVq1DrwBjzJl4o4zpHsePRkxnWru1rh3vXH9QPV";
+        private string accessSecret   = "hem7LrhhE9LK569vNVcaIApDko1Fk5CIDoUlxl9eiIPe3";
+
+        private HotKey hotkey;
+
         public MainWindow()
         {
             InitializeComponent();
+            TwitterCredentials.SetCredentials(accessToken, accessSecret, consumerKey, consumerSecret);
+            this.Hide();
+            InitializeBinding();
+        }
+
+        private void InitializeBinding()
+        {
+            this.hotkey = new HotKey(Key.ImeConvert, KeyModifier.None, OnHotKeyHandler);
+        }
+
+        private void clickedTweetButton(object sender, RoutedEventArgs e)
+        {
+            string text = TweetBox.Text;
+            Tweetinvi.Core.Interfaces.ITweet tweet = Tweet.PublishTweet(text);
+            if (tweet.IsTweetPublished)
+            {
+                TweetBox.Clear();
+                Hide();
+            }
+            else
+            {
+                Console.WriteLine("Something Wrong!");
+            }
+        }
+
+        private void OnHotKeyHandler(HotKey hotKey)
+        {
+            if (this.IsActive)
+            {
+                this.Hide();
+            }
+            else
+            {
+                this.Show();
+                this.TweetBox.Clear();
+                this.TweetBox.Focus();
+            }
         }
     }
 }
